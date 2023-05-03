fbcb136622c459dcba821fe1bb4474ed248aa177
Holy-Knight <vnycall74@naver.com>
2023-05-04 03:11:43 +0900

    refactor: Change to No-Op from destroy in the Android BaseViewModel

--- common/src/androidMain/kotlin/com/haeyum/common/BaseViewModel.kt
+++ common/src/androidMain/kotlin/com/haeyum/common/BaseViewModel.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineScope
 
-actual open class BaseViewModel actual constructor(): ViewModel() {
+actual open class BaseViewModel actual constructor() : ViewModel() {
     actual val viewModelScope: CoroutineScope
         get() = (this as ViewModel).viewModelScope
 
-    actual open fun destroy() = onCleared()
+    actual open fun destroy() { /* no-op */ }
 }
